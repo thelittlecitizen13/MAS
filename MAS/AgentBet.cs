e35eb60f6dec8870d1df3d0e3eb4544a17e5cc54
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-25 16:55:37 +0300

    AgentBet class added

---
+++ MAS/AgentBet.cs
@@ -0,0 +1,15 @@
+﻿using MAS.Agents;
+
+namespace MAS
+{
+    public class AgentBet
+    {
+        public int NewPrice { get; set; }
+        public Agent BettingAgent{ get; set; }
+        public AgentBet(int newPrice, Agent agent)
+        {
+            NewPrice = newPrice;
+            BettingAgent = agent;
+        }
+    }
+}
