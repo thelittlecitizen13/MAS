2a7f8c1223f75f69f7bdebe3c03de9feed4c516b
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-25 16:42:03 +0300

    IAuctionItem interface at MAS.Items added

---
+++ MAS.Items/IAuctionItem.cs
@@ -0,0 +1,10 @@
+﻿namespace MAS.Items
+{
+    public interface IAuctionItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int UniqueID { get; set; }
+        public string ToString();
+    }
+}
