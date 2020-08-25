1acdbfbae38c7c096e07913e4b7c62c31962905d
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-25 16:38:46 +0300

    Agent abstract class added

---
+++ MAS.Agents/Agent.cs
@@ -0,0 +1,21 @@
+﻿namespace MAS.Agents
+{
+    public abstract class Agent
+    {
+        public string Name { get; set; }
+        public int Cash { get; set; }
+        public Agent(string name, int cash)
+        {
+            Name = name;
+            Cash = cash;
+        }
+        public void MakeBet()
+        {
+
+        }
+        public bool DoJoin()
+        {
+            return true;
+        }
+    }
+}
