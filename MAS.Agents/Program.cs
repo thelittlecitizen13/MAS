26557c8765126426240bdba537d9e4cbdba02b4a
thelittlecitizen13 <thelittlecitizen13@gmail.com>
2020-08-25 16:34:50 +0300

    MAS.Agents Projects added

---
+++ MAS.Agents/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MAS.Agents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
